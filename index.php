c71debe6cbf5a6d374662dd8869373f868df5b96
Vipin.sharma_ <vipink96744@gmail.com>
2019-01-16 20:39:13 +0530

    90% completed

---
+++ index.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+</head>
+<body>
+	<?php 
+	include "layout/navbar.php";
+
+	?>
+
+	<!-- iphone XR Section -->
+	<div class="iphone-xr">
+		<div class="unit-wrapper text-white">
+			<a href="" class="iphone-xr-unit-link"></a>
+
+			<!-- Text Section -->
+			<div class="unit-text-wrapper">
+				<h3 class="head">iPhone X<span class="boxed">R</span></h3>
+				<h4 class="subhead">All-screen design. Longest battery life ever in an iPhone.
+					<br>
+					Fastest performance. Studio-quality photos.</h4>
+				<div class="iphone-xr-links">
+					<a href="">Learn More ></a>
+					<a href="">Buy ></a>
+					
+				</div>
+			</div>
+			<!-- Text Section Ends-->
+
+			<!-- iphone XR image -->
+			<div class="unit-image-wrapper">
+				<center><a href=""><img src="img/img1.jpg" class="img-fluid iphone-xr-image"></a></center>	
+			</div>
+			<!-- iphone XR image Ends-->
+
+		</div>
+	</div>
+	<!-- iphone XR Section Ends-->
+
+
+	<!-- iphone XS Section -->
+	<div class="iphone-xs">
+		<div class="unit-wrapper text-white">
+			<a href="" class="iphone-xs-unit-link"></a>
+
+			<!-- Text Section -->
+			<div class="unit-text-wrapper">
+				<h3 class="head">iPhone X<span class="boxed">S</span></h3>
+				<h4 class="subhead">Largest Super Retina display. Fastest performance with
+					<br>
+					A12 Bionic. Most secure facial authentication with Face ID.
+					<br>
+					Breakthrough dual cameras with Depth Control.</h4>
+				<div class="iphone-xs-links">
+					<a href="">Learn More ></a>
+					<a href="">Buy ></a>
+					
+				</div>
+			</div>
+			<!-- Text Section Ends-->
+
+			<!-- iphone XS image -->
+			<div class="unit-image-wrapper">
+				<center><img src="img/img2.jpg" class="img-fluid iphone-xs-image"></center>	
+			</div>
+			<!-- iphone XS image Ends-->
+
+		</div>
+	</div>
+	<!-- iphone XS Section Ends-->
+
+	<!-- watch Section -->
+	<div class="watch">
+		<div class="unit-wrapper text-dark">
+			<a href="" class="watch-unit-link"></a>
+
+			<!-- Text Section -->
+			<div class="watch-text-wrapper">
+				<h3 class="head"><i class="fab fa-apple"></i><b>WATCH</b></h3>
+				<h4 class="watch-subhead"><span style="color: #b71e1e; letter-spacing: 2px;">SERIES 4</span>
+					<br>
+					<br>
+					All new. For a better you.</h4>
+				<div class="watch-links">
+					<a href="">Learn More ></a>
+					<a href="">Buy ></a>
+					
+				</div>
+			</div>
+			<!-- Text Section Ends-->
+
+			<!-- watch image -->
+			<div class="watch-image-wrapper">
+				<img src="img/watch.jpg" class="img-fluid watch-image">
+			</div>
+			<!-- watch image Ends-->
+
+		</div>
+	</div>
+	<!-- watch Section Ends-->
+
+
+	<div class="container-fluid first_deck">
+		<div class="card-deck">
+			<div class="card" style="text-align: center; background-color: #f3f1f1; height: 580px;">
+					<h2 class="card-head"><b>iPad Pro</b></h2>
+					<h5 class="card-subhead">All new. All screen. All powerful.</h5>
+					<div class="links">
+						<a href="">Learn More ></a>
+						<a href="">Buy ></a>
+					</div>
+					<img src="img/ipad_pro.png" class="img-fluid ipad_pro" >
+			</div>
+
+			<div class="card" style="text-align: center; background-color: #f3f1f1; height: 580px;">
+					<h2 class="card-head"><b>MacBook Air</b></h2>
+					<h5 class="card-subhead">Lightness strikes again.</h5>
+					<div class="links">
+						<a href="">Learn More ></a>
+						<a href="">Buy ></a>
+					</div>
+					<img src="img/macbook_air.png" class="img-fluid macbook_air">
+			</div>
+		</div>
+	</div>
+
+
+	<div class="container-fluid second_deck">
+		<div class="card-deck">
+			<div class="card" style="text-align: center; background-color: #fff;">
+					<h3 class="card-head"><b>AirPods</b></h3>
+					<h4 class="card-subhead">Wireless. Effortless. Magical.</h4>
+					<div class="links">
+						<a href="">Learn More ></a>
+						<a href="">Buy ></a>
+					</div>
+					<img src="img/airpods.jpg" class="img-fluid">
+			</div>
+
+			<div class="card" style="text-align: center; color: white; background-image: url(img/music.png); background-size:cover; height: 580px;">
+					<h3 class="card-head"><b><i class="fab fa-apple"></i> Music</b></h3>
+					<h4 class="card-subhead">Lose youself in 50 million songs.</h4>
+					<div class="links">
+						<a href="">Try it free* ></a>
+						<a href="">Learn More ></a>
+					</div>
+					<!-- <img src="img/music.png" class="img-fluid"> -->
+			</div>
+		</div>
+	</div>
+
+
+	<div class="container-fluid third_deck">
+		<div class="card-deck" style=" color: white;">
+			<div class="card" style="text-align: center; background-image: url(img/facetime.jpg); background-size: cover; height: 580px;">
+					<h3 class="card-head"><b>Group FaceTime.</b>
+						<br>Now on ios 12.1.
+					</h3>
+					<div class="links">
+						<a href="">Watch the Film</a>
+						<a href="">How to use Group FaceTime ></a>
+					</div>
+			</div>
+
+			<div class="card" style="text-align: center; background-image: url(img/tv.jpg); background-size: cover; height: 580px;">
+					<h3 class="card-head"><b><i class="fab fa-apple"></i> tv 4K</b></h3>
+					<h4 class="card-subhead">Cinematic in every sense.</h4>
+					<div class="links">
+						<a href="">Learn More ></a>
+						<a href="">Buy ></a>
+					</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container-fluid footer" style="background-color:#f3f1f1; ">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-3 col-lg-3 col-sm-3 list" >
+					<b>Explore</b><br>
+					<a href="">Mac</a><br>
+					<a href="">iPad</a><br>
+					<a href="">iPhone</a><br>
+					<a href="">Watch</a><br>
+					<a href="">TV</a><br>
+					<a href="">Music</a><br>
+					<a href="">iTunes</a><br>
+					<a href="">iPad touch</a><br>
+				</div>
+
+				<div class="col-md-3 col-lg-3 col-sm-3 list">
+					<b>For Education</b><br>
+					<a href="">Apple and Education</a><br>
+					<br>
+
+					<b>For Business</b><br>
+					<a href="">Apple and Business</a>	
+				</div>
+
+				<div class="col-md-3 col-lg-3 col-sm-3 list">
+					<b>Account</b>
+					<br>
+					<a href="">Manage Your Apple ID</a><br>
+					<a href="">iCloud.com</a>	
+					<br>
+					<br>
+					<b>Apple Values</b>
+					<br>
+					<a href="">Accessibility</a><br>	
+					<a href="">Environment</a><br>
+					<a href="">Privacy</a><br>
+					<a href="">Supplier Responsibility</a>
+				</div>
+
+				<div class="col-md-3 col-lg-3 col-sm-3 list">
+				<b>About Apple</b><br>
+				<a href="">Newsroom</a>	<br>
+				<a href="">Apple Leadership</a><br>	
+				<a href="">Job Opportunities</a><br>
+				<a href="">Investors</a><br>
+				<a href="">Events</a><br>
+				<a href="">Contact Apple</a><br>
+				</div>
+			</div>
+			<br>
+			<br>
+			<p>Where to buy: <a href="">find a retailer.</a></p>
+			<hr>
+			<pre style="color: grey;">Copyright © 2019 Apple Inc. All rights reserved.     <a href="" style="color: grey;">Privacy Policy</a>     <a href="" style="color: grey;">Terms of Use</a>       <a href="" style="color: grey;">Legal</a>       <a href="" style="color: grey;">Site Map</a>
+
+			</pre>
+		</div>
+
+		
+	</div>
+
+
+</body>
+</html>
---
+++ layout/navbar.php
@@ -0,0 +1,46 @@
+<nav class="navbar navbar-expand-lg navbar iphone-navbar">
+  <button class="navbar-toggler btn btn-light" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  <a class="navbar-brand" href="#"><i class="fab fa-apple"></i></a>
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  <div class="collapse navbar-collapse navbar-list" id="navbarTogglerDemo03">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Mac <span class="sr-only">(current)</span></a>
+      </li>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <li class="nav-item">
+        <a class="nav-link" href="#">iPad</a>
+      </li>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <li class="nav-item">
+			<a href="" class="nav-link">iPhone</a>
+		</li>
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		<li class="nav-item">
+			<a href="" class="nav-link">Watch</a>
+		</li>
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		<li class="nav-item">
+			<a href="" class="nav-link">Tv</a>
+		</li>
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		<li class="nav-item">
+			<a href="" class="nav-link">Music</a>
+		</li>
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		<li class="nav-item">
+			<a href="" class="nav-link">support</a>
+		</li>
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		<li class="nav-item">
+			<a href="" class="nav-link">Where to Buy</a>
+		</li>
+      
+    </ul>
+	 <i class="fas fa-search" style="color: white; margin-right: 150px;"></i>
+  </div>
+</nav>
